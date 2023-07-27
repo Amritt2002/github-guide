978408ae1474bf6c0714577b6cafa6fa1b316a18
Amritt2002 <112689768+Amritt2002@users.noreply.github.com>
2023-07-27 15:42:25 +0530

    assignment.py
    Assignment for the PMKVY .
    Perfoming different operation on strings, dictionary, list etc.

---
+++ assignment.py
@@ -0,0 +1,116 @@
+# create string type variables
+
+name = "Python"
+print(name)
+
+message = "programming is best."
+print(message)
+
+greet = 'hello'
+
+# access 1st index element
+print(greet[1]) #"e"
+
+greet = 'Namste'
+
+# access 4th last element
+print(greet[-4]) # "e"
+
+# multiline string
+message = """
+Never gonna give you up
+Never gonna let you down
+"""
+
+print(message)
+str1 = "Hello, world!"
+str2 = "I love Python."
+str3 = "Hello, world!"
+
+# compare str1 and str2
+print(str1 == str2)
+
+# compare str1 and str3
+print(str1 == str3)
+
+greet = "Hello, "
+name = "Jack"
+
+# using + operator
+result = greet + name
+print(result)
+
+# Output: Hello, Jack
+
+#creating list 
+#string
+a = "Amritanshu"
+#list
+b=(1,2,5,6, "ak",7.9,4+5j)
+#float
+c= (5.5,9.9,8.8)
+#tuple
+d=("apple","banana","gauava","grapes")
+print(a)
+print(b)
+print(c)
+print(d)
+
+# Creating a Dictionary
+# with Integer Keys
+Dict = {1: 'dont', 2: 'be', 3: 'lazy'}
+print("\nDictionary with the use of Integer Keys: ")
+print(Dict)
+
+# Creating a Dictionary
+# with Mixed keys
+Dict = {'Amritanshu': 'is', 1: [1, 2, 3, 4]}
+print("\nDictionary with the use of Mixed Keys: ")
+print(Dict)
+
+# escape double quotes
+example = "He said, \"What's there?\""
+
+# escape single quotes
+example = 'He said, "What\'s there?"'
+
+print(example)
+
+# Output: He said, "What's there?"
+
+print('a' in 'program') # True
+print('at' not in 'battle') #False
+
+message = 'Hola Amigos'
+
+# assign new string to message variable
+message = 'Hello Friends'
+
+prints(message); # prints "Hello Friends"
+
+str1 = "Hello, world!"
+str2 = "I love Python."
+str3 = "Hello, world!"
+
+# compare str1 and str2
+print(str1 == str2)
+
+# compare str1 and str3
+print(str1 == str3)
+
+# Creating an empty Dictionary
+Dict = {}
+print("Empty Dictionary: ")
+print(Dict)
+
+# Creating a Dictionary
+# with dict() method
+Dict = dict({1: 'Geeks', 2: 'For', 3: 'Geeks'})
+print("\nDictionary with the use of dict(): ")
+print(Dict)
+
+# Creating a Dictionary
+# with each item as a Pair
+Dict = dict([(1, 'Geeks'), (2, 'For')])
+print("\nDictionary with each item as a pair: ")
+print(Dict)
